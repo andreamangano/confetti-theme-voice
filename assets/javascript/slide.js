cb6a45a8a76c19a7ed93aceb0b7b548af717fb92
AMangano <andreaman87@gmail.com>
2017-01-12 20:34:52 +0100

    Adds keyboard slide navigation

---
+++ assets/javascript/slide.js
@@ -0,0 +1,14 @@
+/*
+  It takes the object navigation (from the template data) printed into the slide page.
+*/
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  // Check left arrow pressing
+  if (evt.keyCode == 37 && navigation.prev) {
+    window.location = navigation.prev.data.url;
+  }
+  // Check right arrow pressing
+  if (evt.keyCode == 39 && navigation.next) {
+    window.location = navigation.next.data.url;
+  }
+};
