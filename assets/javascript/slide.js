12f86439cc8776415c093800f9f7b06840cf7291
AMangano <andreaman87@gmail.com>
2017-01-13 20:24:59 +0100

    Adds on dom loaded

--- assets/javascript/slide.js
+++ assets/javascript/slide.js
@@ -1,14 +1,16 @@
-/*
-  It takes the object navigation (from the template data) printed into the slide page.
-*/
-document.onkeydown = function(evt) {
-  evt = evt || window.event;
-  // Check left arrow pressing
-  if (evt.keyCode == 37 && navigation.prev) {
-    window.location = navigation.prev.data.url;
-  }
-  // Check right arrow pressing
-  if (evt.keyCode == 39 && navigation.next) {
-    window.location = navigation.next.data.url;
-  }
-};
+(function() {
+  /*
+   It takes the object navigation (from the template data) printed into the slide page.
+   */
+  document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    // Check left arrow pressing
+    if (evt.keyCode == 37 && navigation.prev) {
+      window.location = navigation.prev.data.url;
+    }
+    // Check right arrow pressing
+    if (evt.keyCode == 39 && navigation.next) {
+      window.location = navigation.next.data.url;
+    }
+  };
+})();
